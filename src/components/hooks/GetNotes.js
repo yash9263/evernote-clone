589fc6ec945ef98189b5086ee4e99cc7cf094c93
yash9263 <yashwant8530@gmail.com>
2021-05-17 22:14:29 +0530

    update in getnotes

--- src/components/hooks/GetNotes.js
+++ src/components/hooks/GetNotes.js
@@ -1,22 +1,25 @@
-import React, {useState, useEffect} from 'react';
-import firebase from 'firebase/app'
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/app";
 
 const GetNotes = (collection) => {
-    const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-    useEffect (() => {
-        const unsub = firebase.firestore().collection(collection).onSnapshot((snap) => {
-            let documents = [];
-            snap.forEach((doc) => {
-                documents.push({...doc.data(), id: doc.id});
-            })
-            setNotes(documents);
-        })
+  useEffect(() => {
+    const unsub = firebase
+      .firestore()
+      .collection(collection)
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setNotes(documents);
+      });
 
-        return () => unsub();
-    }, [collection])
+    return () => unsub();
+  }, [collection]);
 
-    return {notes};
-}
+  return { notes };
+};
 
 export default GetNotes;
